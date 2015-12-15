040b5ff5c96eae4efa5ef1da81daff3f2fcfcebc
Matthew Wilcoxson <matthew.wilcoxson@bodleian.ox.ac.uk>
2015-12-15 16:10:15 +0000

    Simple web interface to display annalist data.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var config = require('../lib/config');
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	MongoClient.connect(config.local.databaseUrl, function(err, db) {
+		if (err) {
+			throw err;
+		}
+		db.collection('Carolan').find({}).sort({"annal:type_id":1,"rdfs:label":1}).toArray(function(err, result) {
+			if (err) {
+				throw err;
+			}
+			res.render('index', { title: 'Express', results: result });
+		});
+	});
 });
 
+/* GET home page. */
+router.get('/:id', function(req, res, next) {
+	MongoClient.connect(config.local.databaseUrl, function(err, db) {
+		if (err) {
+			throw err;
+		}
+		db.collection('Carolan').find({_id:new mongodb.ObjectId(req.params.id)}).toArray(function(err, results) {
+			if (err) {
+				throw err;
+			}
+			if( results.length > 0 ) {
+				res.render('item', { found: true, result: results[0] } );
+			}
+			else {
+				res.render('item', {found:false, result:null} );
+			}
+		});
+	});
+});
 module.exports = router;
