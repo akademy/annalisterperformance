1c0890b928e59441ede17c4f8e9530560d9bdf87
Matthew Wilcoxson <bitbucket@akademy.co.uk>
2016-01-10 14:38:48 +0000

    Complete import

--- bin/dataload-file.js
+++ bin/dataload-file.js
@@ -12,7 +12,9 @@ var dataFile = "entity_data.jsonld";
 var MongoClient = require('mongodb').MongoClient;
 
 //var fileRoot = '/home/annalist/annalist_site/c/Performance_defs/d/';
-var fileRoot = '/home/matthew/Dropbox/code/IntelliJ/AnnalistPerformance/node_modules/';
+//var fileRoot = '/home/matthew/Dropbox/code/IntelliJ/AnnalistPerformance/node_modules/';
+var fileRoot = '/home/matthew/AnnalisterPerformance/temp/data/annalist_site/c/';
+var repoRoot = 'Performance_defs/d/'
 /*
  Connect to database and request filelist in annalist (via apache2 folder view)
  */
@@ -24,11 +26,12 @@ MongoClient.connect( config.local.databaseUrl, function(error, db) {
 
 	db.createCollection('Performance', function(err, collection) {
 
-		var rootFiles = fs.readdirSync( fileRoot );
+		var rootFilesPath = path.join( fileRoot, repoRoot );
+		var rootFiles = fs.readdirSync( rootFilesPath );
 
 		rootFiles.forEach( function( file ) {
 
-			var folderPath = path.join( fileRoot, file );
+			var folderPath = path.join( rootFilesPath, file );
 			var stat = fs.statSync( folderPath );
 
 			if( stat.isDirectory() ) {
@@ -38,7 +41,7 @@ MongoClient.connect( config.local.databaseUrl, function(error, db) {
 				pathFiles.forEach( function( file ) {
 
 					var subPath = path.join( folderPath, file );
-					console.log( subPath );
+					//console.log( subPath );
 
 					var stat = fs.statSync( subPath );
 
@@ -47,16 +50,19 @@ MongoClient.connect( config.local.databaseUrl, function(error, db) {
 						var filePath = path.join( subPath, dataFile );
 						console.log( filePath );
 
-						//var fileContents = fs.readFileSync(filePath);
-						//console.log(fileContents[25]);
+						var fileContents = fs.readFileSync(filePath,"utf8");
+						//console.log(fileContents.substr(0,25));
 
-						//upsertDocument( fileContents, collection );
+						var url = filePath.replace( new RegExp("^"+fileRoot),"").replace(new RegExp(dataFile+"$"),"");
+						upsertDocument( url, fileContents, collection );
 					}
 				});
 
 
 			}
 		});
+
+		//todo : FIX THIS! db.close(); -- I need to rewrite it ...
 	});
 });
 
@@ -67,7 +73,7 @@ function jsonldToMongo( collection, dataRoot, dataSub, dataUrls ) {
 
 		var url = dataRoot + dataSub + dataUrl + dataFile;
 
-		//	console.log(url);
+//      console.log(url);
 
 		request(url, function (error, response, body) {
 
@@ -81,7 +87,7 @@ function jsonldToMongo( collection, dataRoot, dataSub, dataUrls ) {
 	});
 }
 
-function upsertDocument( document, collection ) {
+function upsertDocument( url, document, collection ) {
 	// TODO: var ok = response.statusCode == 200;
 	var bodyJson = null;
 	try {
@@ -93,17 +99,19 @@ function upsertDocument( document, collection ) {
 		var annalType = bodyJson["annal:type_id"],
 			annalId = bodyJson["annal:id"];
 
-		bodyJson["databaseInsertDate"] = new Date() * 1;
+		bodyJson["databaseInsertDate"] = new Date();
 		bodyJson["annalistUrl"] = url;
 
 		collection.replaceOne({
-			"annal:type_id": annalType,
-			"annal_id": annalId
-		}, bodyJson, {upsert: true}, function (error, record) {
-			if (error) {
-				console.error(error);
-			}
-			console.log(record);
-		})
+				"annalistUrl" : url
+			},
+			bodyJson,
+			{upsert: true},
+			function (error, record) {
+				if (error) {
+					console.error(error);
+				}
+				console.log(record);
+			})
 	}
 }
